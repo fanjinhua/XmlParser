6e0f85892ae60b9715eeaf9f8a42bc7954059f5a
jfanner <fanyiyia@gmail.com>
2017-04-04 18:18:43 +0800

    commit

--- XmlParser/utils.h
+++ XmlParser/utils.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <stdexcept>
+#include <string>
 
 namespace xml
 {
 namespace DOM
 {
+	using std::string;
 	class DOMException : public std::exception
 	{
 	public:
@@ -17,6 +19,33 @@ namespace DOM
 		}
 	private:
 		const char* info_;
+	};
+
+	class ParseException : public DOMException
+	{
+	public:
+		ParseException(const char* info) : DOMException(info) {}
+	};
+
+	class tokenizer
+	{
+	public:
+		std::string& tokenize(std::string data)
+		{
+			/* empty_element tags*/
+			for (std::string::size_type index = 0; index != data.size(); ++index)
+			{
+				if (data[index] == '<')
+				{
+					string name;
+					if (data[index + 1] == ' ')  // TODO, whitespace
+						throw ParseException("WHITESPACE_FAULT");
+					for (auto j = index; data[j] != ' '; ++j)
+						name.push_back(data[j]);
+
+				}
+			}
+		}
 	};
 }
 }
